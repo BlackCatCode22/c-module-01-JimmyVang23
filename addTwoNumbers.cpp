acb7e09b6205b392fddb77fae1577a190f929ad2
JimmyVang23 <touteevang@gmail.com>
2025-08-13 12:25:00 -0700

    Add files via upload

---
+++ addTwoNumbers.cpp
@@ -0,0 +1,18 @@
+// Created by Jimmy on 8/13/2025.
+// addTwoNumbers.cpp
+
+#include <iostream>
+using namespace std;
+
+//create a very important function named main
+//this is where your program startes
+
+int main() {
+    int sumOFNums = 0;
+    int num1 = 0;
+    int num2 = 0;
+
+cout << 'Enter First Number: ";
+num1 = cin.get();
+    return 0;
+}
