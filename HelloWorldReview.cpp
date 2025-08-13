ac86671b10e6ab2c08bce7882faa4e15a81801df
JimmyVang23 <touteevang@gmail.com>
2025-08-13 11:35:46 -0700

    Add files via upload

---
+++ HelloWorldReview.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World Review" << endl;
+
+int a = 22;
+int num2 = 23;
+int sum = a + num2;
+std::cout << " the sum of " << a << " and " << num2 << " = " << sum << endl;
+
+    return 0;
+}
